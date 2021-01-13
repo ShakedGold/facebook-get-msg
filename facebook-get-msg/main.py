a60cdba35db866846e417cd922e3eaa4855f55a6
Appletee <shakedgold2005@gmail.com>
2021-01-13 22:31:53 +0200

    added groups

--- facebook-get-msg/main.py
+++ facebook-get-msg/main.py
@@ -13,21 +13,30 @@ class Encoder(json.JSONEncoder):
 
 
 def post_text_image():
-    user = input("Enter link to page or user: ")
-    field = user.split("/")[3]
-    if user.split("/")[2] == "www.facebook.com":
-        for post in get_posts(field, pages=1):
-            get = post.get("post_text")
-            post_get = post.get("image")
-            if post["image"] is not None:
-                short = Shortener().tinyurl.short(post_get)
-                print(f'Post Text: {get}\n\nImage: {short}\n')
-            else:
-                print("Post Text:", get, "\n\nImage:", post_get, "\n")
+    user = input("Enter link to groups, users or pages: ")
+    fields = user.split("/")
+    if fields[2] == "www.facebook.com":
+        if fields[3] == "groups":
+            for post in get_posts(pages=1, group=fields[4]):
+                get = post.get("post_text")
+                post_get = post.get("image")
+                if post["image"] is not None:
+                    short = Shortener().tinyurl.short(post_get)
+                    print(f'Post Text: {get}\n\nImage: {short}\n')
+                else:
+                    print("Post Text:", get, "\n\nImage:", post_get, "\n")
+        else:
+            for post in get_posts(fields[3], pages=1):
+                get = post.get("post_text")
+                post_get = post.get("image")
+                if post["image"] is not None:
+                    short = Shortener().tinyurl.short(post_get)
+                    print(f'Post Text: {get}\n\nImage: {short}\n')
+                else:
+                    print("Post Text:", get, "\n\nImage:", post_get, "\n")
     else:
         print("not a valid facebook link\n")
         post_text_image()
 
 
 post_text_image()
-# json.dumps(post, ensure_ascii=False, cls=Encoder, indent=4)
