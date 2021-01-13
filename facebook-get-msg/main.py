6e2042132af0e41ab4b7201b5a32af53954c87f1
Appletee <shakedgold2005@gmail.com>
2021-01-13 22:18:39 +0200

    fixed bugs + added shorter url

--- facebook-get-msg/main.py
+++ facebook-get-msg/main.py
@@ -1,4 +1,5 @@
 from facebook_scraper import get_posts
+from pyshorteners import Shortener
 import json
 import datetime
 
@@ -11,12 +12,22 @@ class Encoder(json.JSONEncoder):
             return json.JSONEncoder.default(self, obj)
 
 
-user = input("Enter link to page or user: ")
-field = user.split("/")[3]
-if user.split("/")[2] == "www.facebook.com":
-    for post in get_posts(field, pages=1):
-        print("Post Text:", post["post_text"], "\n", "Image:", post["image"], "\n")
-else:
-    print("not a valid facebook link")
+def post_text_image():
+    user = input("Enter link to page or user: ")
+    field = user.split("/")[3]
+    if user.split("/")[2] == "www.facebook.com":
+        for post in get_posts(field, pages=1):
+            get = post.get("post_text")
+            post_get = post.get("image")
+            if post["image"] is not None:
+                short = Shortener().tinyurl.short(post_get)
+                print(f'Post Text: {get}\n\nImage: {short}\n')
+            else:
+                print("Post Text:", get, "\n\nImage:", post_get, "\n")
+    else:
+        print("not a valid facebook link\n")
+        post_text_image()
 
+
+post_text_image()
 # json.dumps(post, ensure_ascii=False, cls=Encoder, indent=4)
